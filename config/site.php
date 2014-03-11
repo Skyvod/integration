8b6a9649626d9d2ba81e15d6308d8329d0bde4e3
fournier <elyriand@gmail.com>
2014-03-11 14:44:56 -0700

    Source finale

---
+++ config/site.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_SERVER', 'mysql7.000webhost.com');
+define('DB_USERNAME', 'a4786031_skyvod');
+define('DB_PASSWORD', 'lolilol62');
+define('DB_DATABASE', 'a4786031_iut');
+define('PASSWORD_SALT', 'TP6MXg7zN4UnlNav6FBmZcMg1hRt1fV8TYiVih5SlloRDqKZ7xZxPSaIDsQYnRwK');
